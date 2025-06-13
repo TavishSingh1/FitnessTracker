ac395fdcc664c74a23125a75f835d847292818e8
VishwajithS2005 <vishwajiths2005@gmail.com>
2025-06-13 13:15:17 +0530

    Incorporated MongoDB with the project.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,5 +15,6 @@ app.get('/', async (req, res) => {
 })
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on http://localhost:${PORT}`);
 });
